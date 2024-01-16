5c7c059f9b6f2243df134de780ac7d9c6708af78
LennertVG <LennertVanGenechten14@gmail.com>
2024-01-16 12:02:42 +0100

    added token functionality to user requests

--- routes/api.php
+++ routes/api.php
@@ -48,19 +48,44 @@ Route::get('/stockitems', function () {
    return response()->json($results);
 });
 
- 
-//  Users
+ // Routes and endpoints for users
  Route::get('/users', function () {
-    return DB::table('users')->get();
+   $users = DB::table('users')->get();
+   return response()->json($users);
  });
 
+//  POST-method for inserting new registered user
  Route::post('/users', function (Request $request) {
-   $name = $request->name;
-   $email = $request->email;
-   $password = $request->password;
- 
-   DB::insert('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [$name, $email, $password]);
-   return response()->json(['message' => 'registered successfully'], 201);
+  $validatedData = $request->validate([
+      'name' => 'required|max:255',
+      'email' => 'required|email|unique:users',
+      'password' => 'required',
+  ]);
+//   Upon creating a new entry in the user table => insert timestamp
+  $user = User::create([
+      'name' => $validatedData['name'],
+      'email' => $validatedData['email'],
+      'password' => $validatedData['password'],
+      'created_at' => now(),
+      'updated_at' => now(),
+  ]);
+  // Ensure the User model is using the HasApiTokens trait
+  $token = $user->createToken('auth_token')->plainTextToken;
+  // Update the remember_token in the database with the new token
+  DB::table('users')
+      ->where('id', $user->id)
+      ->update(['remember_token' => $token]);
+// Display message and return json for $token
+  return response()->json(['id' => $user->id, 'token' => $token], 201);
+});
+ // token routes
+ Route::post('/tokens/create', function (Request $request) {
+    $user = User::find($user->id);
+    //return $user;
+    $token = $user->createToken('mynewtoken');
+    return ['token' => $token->plainTextToken];
+    // post the plaintexttoken to the user in the database
+    $user:: where('id', $user->id)->update(['remember_token' => $token->plainTextToken]);
  });
 
 
